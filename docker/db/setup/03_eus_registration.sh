8fc96a56c487bc1fb029f55085117c8926c595b6
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-03-20 06:14:11 +0100

    clean up and move Docker TVD Labs config

--- docker/db/setup/03_eus_registration.sh
+++ docker/db/setup/03_eus_registration.sh
@@ -3,7 +3,7 @@
 # Trivadis AG, Business Development & Support (BDS)
 # Saegereistrasse 29, 8152 Glattbrugg, Switzerland
 # -----------------------------------------------------------------------
-# Name.......: 03_eus_config.sh
+# Name.......: 03_eus_registration.sh
 # Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
 # Editor.....: Stefan Oehrli
 # Date.......: 2018.06.27
@@ -46,4 +46,10 @@ $ORACLE_HOME/bin/dbca -silent -configureDatabase \
 -sourceDB ${ORACLE_SID} -sysDBAUserName sys -sysDBAPassword $(cat ${SYS_PWD_FILE}) \
 -registerWithDirService true -dirServiceUserName ${EUS_ADMIN} \
 -dirServicePassword $(cat ${EUS_PWD_FILE}) -walletPassword $(cat ${WALLET_PWD_FILE}) 
+
+# - unlock system -------------------------------------------------------
+$ORACLE_HOME/bin/sqlplus -S -L /NOLOG <<EOFSQL
+CONNECT / AS SYSDBA
+ALTER USER system IDENTIFIED BY $(cat ${SYS_PWD_FILE}) ACCOUNT UNLOCK;
+EOFSQL
 # - EOF -----------------------------------------------------------------
