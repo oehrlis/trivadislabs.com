fcb2243ba248d8f210e863d77bfed01ae76fd9be
Stefan Oehrli <stefan.oehrli@trivadis.com>
2019-04-29 11:15:17 +0200

    add pdb os user test case

--- vagrant/config/os/06_add_pdb_os_user.sh
+++ vagrant/config/os/06_add_pdb_os_user.sh
@@ -23,19 +23,37 @@ PDB_OS_PASSWD=manager
 PDB_OS_GRP=restricted
 # - End of Customization ------------------------------------------------
 
-# - configure instance --------------------------------------------------
-echo "Add PDB OS User:"
-echo "  PDB_OS_USER         :   ${PDB_OS_USER}"
-echo "  PDB_OS_GRP          :   ${PDB_OS_GRP}"
+# Make sure only root can run our script
+if [ $EUID -ne 0 ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
 
+# if neccessary install the password utility
+if [ -n "$(command -v passwd)" ]; then 
+    echo "Found passwd utility"
+else 
+    echo "Install passwd and set password for ${PDB_OS_USER}:"
+    yum install -y passwd
+    yum clean all
+    rm -rf /var/cache/yum
+fi
+
+# create OS group
 echo "Add PDB OS group ${PDB_OS_GRP}:"
 groupadd restricted
 
-echo "Add PDB OS user ${PDB_OS_USER}:"
-useradd --create-home --gid restricted --shell /bin/bash orapdb
+# create a bunch of OS user
+for i in oracdb orapdb1 orapdb2 orapdb3; do
+    PDB_OS_USER=${i}
 
-echo "Install passwd and set password for ${PDB_OS_USER}:"
-yum install -y passwd
-yum clean all
-echo "${PDB_OS_PASSWD}" | passwd orapdb --stdin 
+    # - configure instance --------------------------------------------------
+    echo "Add PDB OS User:"
+    echo "  PDB_OS_USER         :   ${PDB_OS_USER}"
+    echo "  PDB_OS_GRP          :   ${PDB_OS_GRP}"
+
+    echo "Add PDB OS user ${PDB_OS_USER}:"
+    useradd --create-home --gid restricted --shell /bin/bash ${PDB_OS_USER}
+    echo "${PDB_OS_PASSWD}" | passwd ${PDB_OS_USER} --stdin 
+done
 # - EOF -----------------------------------------------------------------
