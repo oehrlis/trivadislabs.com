323da7c838f056077cd62644f1591ca3fb9f62fc
Stefan Oehrli <stefan.oehrli@trivadis.com>
2020-02-20 05:45:43 +0100

    update oud build scripts

--- ol7oud12/scripts/10_install_binaries.sh
+++ ol7oud12/scripts/10_install_binaries.sh
@@ -21,41 +21,75 @@
 # - just add/update any kind of customized environment variable here
 # define variables for OS setup
 SETUP_JAVA="01_setup_os_java.sh"
-SETUP_OUD="10_setup_oud_12c.sh"
+SETUP_OUD="10_setup_oud.sh"
 SETUP_OUDSM="10_setup_oudsm_12c.sh"
 SETUP_OUDBASE="20_setup_oudbase.sh"
-JAVA_PKG="p29565620_180212_Linux-x86-64.zip"
+OUD_VERSION="12"
+OUD_TYPE="OUDSM12" 
+
 # - End of Customization ----------------------------------------------------
 # - Environment Variables ---------------------------------------------------
 # source values from vagrant YAML file
 . /vagrant_common/scripts/00_init_environment.sh
 export DEFAULT_DOMAIN=${domain_name:-"trivadislabs.com"} 
+# set the software packages for OUD and OUDSM
+export JAVA_PKG=${JAVA_PKG:-"p30425892_180241_Linux-x86-64.zip"}
+export OUD_BASE_PKG=${OUD_BASE_PKG:-"p30188352_122140_Generic.zip"}
+export FMW_BASE_PKG=${FMW_BASE_PKG:-"fmw_12.2.1.4.0_infrastructure_Disk1_1of1.zip"}
+export OUD_PATCH_PKG=${OUD_PATCH_PKG:-""}
+export FMW_PATCH_PKG=${FMW_PATCH_PKG:-"p30689820_122140_Generic.zip"}
+export OUD_OPATCH_PKG=${OUD_OPATCH_PKG:-""}
+export OUI_PATCH_PKG=${OUI_PATCH_PKG:-""}
+export OUD_ORACLE_HOME_NAME=${OUD_ORACLE_HOME_NAME:-"oud12.2.1.4.0"}
+export OUDSM_ORACLE_HOME_NAME=${OUDSM_ORACLE_HOME_NAME:-"fmw12.2.1.4.0"}
+
 # - EOF Environment Variables -----------------------------------------------
 
 # - Main --------------------------------------------------------------------
 echo "= Start part 10 ======================================================="
 echo "- Setup Java -----------------------------------------------------------"
 # Setup Java
-su -l oracle -c "export JAVA_PKG=${JAVA_PKG}; ${ORADBA_BIN}/${SETUP_JAVA}"
+su -l oracle -c "JAVA_PKG=${JAVA_PKG} ${ORADBA_BIN}/${SETUP_JAVA}"
+
+echo "- Config Java ----------------------------------------------------------"
+JAVA_DIR=$(ls -1 -d ${ORACLE_BASE}/product/jdk*|tail -1)
+su -l oracle -c "cp /vagrant/config/java.security ${JAVA_DIR}/jre/lib/security/java.security"
 
 echo "- Setup OUD -----------------------------------------------------------"
 # Install OUD Software
-su -l oracle -c "${ORADBA_BIN}/${SETUP_OUD}"
+su -l oracle -c "ORACLE_HOME_NAME=${OUD_ORACLE_HOME_NAME} \
+OUD_TYPE=OUD12 \
+OUD_BASE_PKG=${OUD_BASE_PKG}  \
+OUD_PATCH_PKG=${OUD_PATCH_PKG}  \
+OUD_OPATCH_PKG=${OUD_OPATCH_PKG}  \
+OUI_PATCH_PKG=${OUI_PATCH_PKG} \
+${ORADBA_BIN}/${SETUP_OUD}"
 
 echo "- Setup OUDSM ---------------------------------------------------------"
 # Install OUDSM Software
-su -l oracle -c "${ORADBA_BIN}/${SETUP_OUDSM}"
+su -l oracle -c "ORACLE_HOME_NAME=${OUDSM_ORACLE_HOME_NAME} \
+OUD_TYPE=OUDSM12 \
+OUD_BASE_PKG=${OUD_BASE_PKG} \
+FMW_BASE_PKG=${FMW_BASE_PKG} \
+OUD_PATCH_PKG=${OUD_PATCH_PKG} \
+FMW_PATCH_PKG=${FMW_PATCH_PKG} \
+OUD_OPATCH_PKG=${OUD_OPATCH_PKG} \
+OUI_PATCH_PKG=${OUI_PATCH_PKG} \
+${ORADBA_BIN}/${SETUP_OUD}"
 
 echo "- OUD Base ------------------------------------------------------------"
 # Install OUD Base
 su -l oracle -c "${ORADBA_BIN}/${SETUP_OUDBASE}"
 
+echo "oud_eus:1389:1636:4444:8989:OUD:Y" >> ${ORACLE_DATA}/etc/oudtab
+echo "oudsm_domain:7001:7002:::OUDSM:Y" >> ${ORACLE_DATA}/etc/oudtab
+
 # get the git stuff
 # echo "--- Get git stuff -----------------------------------------------------"
-# su -l oracle -c "cd ${ORACLE_BASE}/local;git clone https://github.com/oehrlis/doag2018 doag2018"
-# su -l oracle -c "cd ${ORACLE_BASE}/local;git clone https://github.com/oehrlis/trivadislabs.com trivadislabs.com"
-# su -l oracle -c "echo \"alias git_lab='cd $cdl/trivadislabs.com;git pull; cd -'\" >>${ORACLE_BASE}/local/oudbase/etc/oudenv_custom.conf"
-# su -l oracle -c "echo \"alias git_doag='cd $cdl/doag2018;git pull; cd -'\" >>${ORACLE_BASE}/local/oudbase/etc/oudenv_custom.conf"
+su -l oracle -c "cd ${ORACLE_BASE}/local;git clone https://github.com/oehrlis/doag2018 doag2018"
+su -l oracle -c "cd ${ORACLE_BASE}/local;git clone https://github.com/oehrlis/trivadislabs.com trivadislabs.com"
+su -l oracle -c "echo \"alias git_lab='cd $cdl/trivadislabs.com;git pull; cd -'\" >>${ORACLE_BASE}/local/oudbase/etc/oudenv_custom.conf"
+su -l oracle -c "echo \"alias git_doag='cd $cdl/doag2018;git pull; cd -'\" >>${ORACLE_BASE}/local/oudbase/etc/oudenv_custom.conf"
 
 echo "= Finish part 10 ======================================================"
 # --- EOF --------------------------------------------------------------------
---
+++ ol7oud12/scripts/20_create_oud_instance.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+# ---------------------------------------------------------------------------
+# Trivadis AG, Infrastructure Managed Services
+# Saegereistrasse 29, 8152 Glattbrugg, Switzerland
+# ---------------------------------------------------------------------------
+# Name.......: 20_create_oud_instance.sh 
+# Author.....: Stefan Oehrli (oes) stefan.oehrli@trivadis.com
+# Editor.....: Stefan Oehrli
+# Date.......: 2018.09.27
+# Revision...: 
+# Purpose....: Wrapper script to create oud instance.
+# Notes......: ...
+# Reference..: --
+# License....: Licensed under the Universal Permissive License v 1.0 as 
+#              shown at http://oss.oracle.com/licenses/upl.
+# ---------------------------------------------------------------------------
+# Modified...:
+# see git revision history for more information on changes/updates
+# ---------------------------------------------------------------------------
+# - Customization -----------------------------------------------------------
+# - just add/update any kind of customized environment variable here
+CREATE_DB="52_create_database.sh"
+# - End of Customization ----------------------------------------------------
+
+# - Environment Variables ---------------------------------------------------
+# source values from vagrant YAML file
+. /vagrant_common/scripts/00_init_environment.sh
+export DEFAULT_DOMAIN=${domain_name:-"trivadislabs.com"}
+# - EOF Environment Variables -----------------------------------------------
+
+# - Main --------------------------------------------------------------------
+echo "= Start part 20 ======================================================="
+
+if [ -n "${ORACLE_SID1}" ]; then
+    echo "- Create DB CDB ${ORACLE_SID1} ----------------------------------------"
+    # Create DB container DB
+    su -l oracle -c "${ORADBA_BIN}/${CREATE_DB} ${ORACLE_SID1} PDB1 TRUE"
+
+    # install the TVD_HR schema
+    su -l oracle -c " . ${ORAENV} ${ORACLE_SID2}; sqlplus /nolog @$TVD_HR_CONTAINER"
+fi
+
+if [ -n "${ORACLE_SID2}" ]; then
+    echo "- Create DB 2DB ${ORACLE_SID2} ----------------------------------------"
+    # Create DB single tenant DB
+    su -l oracle -c "${ORADBA_BIN}/${CREATE_DB} ${ORACLE_SID2} PDB1 FALSE"
+
+    # install the TVD_HR schema
+    su -l oracle -c " . ${ORAENV} ${ORACLE_SID2}; sqlplus /nolog @$TVD_HR"
+    su -l oracle -c " . ${ORAENV} ${ORACLE_SID2}; sqlplus / as sysdba @?/rdbms/admin/utlsampl.sql"
+fi
+echo "--- Configure Oracle Service ------------------------------------------"
+cp ${ORACLE_BASE}/local/dba/etc/oracle.service /usr/lib/systemd/system/
+systemctl --system daemon-reload
+systemctl enable oracle
+
+# change all DB to autostart in oratab
+sed -i "s/N$/Y/" /etc/oratab
+
+echo "= Finish part 20 ======================================================"
+# --- EOF --------------------------------------------------------------------
